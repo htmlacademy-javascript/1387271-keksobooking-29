3bd2366afe86f926d60ced7eabee233d564d97d0
Dik Olga <aolaxy@yandex.ru>
2023-08-10 13:46:16 +0300

    end

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 import {createObjects} from './data.js';
-
 const COUNT_BOOKING = 10;
 const ArrayofObjects = () => Array.from({length:COUNT_BOOKING}, (_, index)=>createObjects(index));
 console.log(ArrayofObjects());
