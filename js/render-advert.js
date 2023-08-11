4cafeb9e81383d801f98f095778bb0e0477263a6
Dik Olga <aolaxy@yandex.ru>
2023-08-11 16:12:46 +0300

    вывод только 1 го элемента(не получается)

--- js/render-advert.js
+++ js/render-advert.js
@@ -1,4 +1,4 @@
-/*
+
 import { createObjects } from './data';
 const WIDTH = 45;
 const HEIGHT = 40;
@@ -36,7 +36,7 @@ const createImage = (objectKey) => {
 };
 
 
-const renderAdvert = ()=>{
+/*const renderAdvert = ()=>{
   const listFragment = document.createDocumentFragment();
   const advertList = createObjects();
 
@@ -85,5 +85,54 @@ const renderAdvert = ()=>{
   advertContainer.appendChild(listFragment);
 
 };*/
-const renderAdvert = ()=>{};
+const renderAdvert = ()=>{
+  const listFragment = document.createDocumentFragment();
+  const advertList = createObjects();
+
+
+  const advertElement = advertTemplate.cloneNode(true);
+  const popupFeatureItems = advertElement.querySelectorAll('.popup__feature');
+  const popupPhotoList = advertElement.querySelector('.popup__photos');
+  const popupPhotos = popupPhotoList.querySelector('.popup__photo');
+
+  Object.keys(advertList.offer).forEach((key) => {
+    if (!key) {
+      advertElement.querySelector(`.popup__${OFFER_KEYS[key]}`).remove();
+    }
+  });
+  advertElement.querySelector('title').textContent = advertList[0].offer.title;
+  advertElement.querySelector('.popup__text--address').textContent = advertList[0].offer.address;
+  advertElement.querySelector('.popup__text--price').firstChild.data = advertList[0].offer.price;
+  advertElement.querySelector('.popup__type').textContent = offerType[advertList[0].offer.type];
+  advertElement.querySelector('.popup__description').textContent = advertList[0].offer.description;
+  advertElement.querySelector('.popup__text--time').textContent = `Заезд после ${advertList[0].offer.checkin} выезд до ${advertList[0].offer.checkout}`;
+  if (advertList[0].offer.features) {
+    const modifiers = advertList[0].offer.features.map((feature) => `popup__feature--${feature}`);
+    popupFeatureItems.forEach((popupFeatureItem) => {
+      const modifier = popupFeatureItem.classList[1];
+      if (!modifiers.includes(modifier)) {
+        popupFeatureItem.remove();
+      }
+    });
+  } else {
+    advertElement.querySelector('.popup__features').remove();
+  }
+  if (advertList[0].offer.photos) {
+    advertList[0].offer.photos.forEach((photo) => popupPhotoList.appendChild(createImage(photo)));
+  } else {
+    advertElement.querySelector('.popup__photos').remove();
+  }
+  popupPhotos.remove();
+
+  if (advertList[0].author.avatar) {
+    advertElement.querySelector('.popup__avatar').src = advertList[0].author.avatar;
+  } else {
+    advertElement.querySelector('.popup__avatar').remove();
+  }
+  listFragment.appendChild(advertElement);
+
+  advertContainer.appendChild(listFragment);
+
+};
+
 export {renderAdvert};
