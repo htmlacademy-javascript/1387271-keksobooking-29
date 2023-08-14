cb04c19a53255a3cb0fb222ba624786331f476a6
Dik Olga <aolaxy@yandex.ru>
2023-08-14 14:34:42 +0300

    добавила ф-цию по активации формы

--- js/form.js
+++ js/form.js
@@ -9,9 +9,22 @@ const disactiveForm = (form)=>{
     element.setAttribute('disabled', 'disabled');
   });
 };
+const activeForm = (form)=>{
+  form.classList.remove(`${form.classList[0]}--disabled`);
+
+  const formChildren = Array.from(form.children);
+  formChildren.forEach((element) => {
+    element.removeAttribute('disabled', 'disabled');
+  });
+};
 
 const toDisactiveForms = ()=>{
   disactiveForm(addformElement);
   disactiveForm(mapfiltersElement);
 };
-export{toDisactiveForms};
+const toActiveForms=()=>{
+  activeForm(addformElement);
+  activeForm(mapfiltersElement);
+};
+
+export{toDisactiveForms,toActiveForms};
