e5970d812f9cf198c44c3ee3f066b26bdd5e1433
Dik Olga <aolaxy@yandex.ru>
2023-08-21 18:44:55 +0300

    добавлена главная метка mainPinMarker

--- js/map.js
+++ js/map.js
@@ -6,6 +6,12 @@ const TOKIO_LAT_LNG_ = {
   lat: 35.65952,
   lng: 139.78179,
 };
+
+const mainPinIcon = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
 let map = null;
 //инициализация карты
 const mapInit = ()=>{
@@ -14,6 +20,11 @@ const mapInit = ()=>{
     .setView(TOKIO_LAT_LNG_, ZOOM);
   L.tileLayer(TILE_LAYER,{
     attribution: ATTRIBUTION,}).addTo(map);
+  const mainPinMarker = L.marker(TOKIO_LAT_LNG_, {
+    draggable: true,
+    icon: mainPinIcon,
+  });
+  mainPinMarker.addTo(map);
 };
 
 export{mapInit};
